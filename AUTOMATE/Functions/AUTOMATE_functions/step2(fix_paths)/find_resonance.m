05097708b41b40296f9a74a709f905093fad32b9
HadrienAFSA <82944710+HadrienAFSA@users.noreply.github.com>
2022-01-05 20:06:32 +0100

    Update find_resonance function
    Added arbitrary resonances options (all n:m combinations up to 3) for outer planets

--- AUTOMATE/Functions/AUTOMATE_functions/step2(fix_paths)/find_resonance.m
+++ AUTOMATE/Functions/AUTOMATE_functions/step2(fix_paths)/find_resonance.m
@@ -1,5 +1,6 @@
 function [list_nm] = find_resonance(pl)
-% List of resonances used. See AUTOMATE report for references
+% List of resonances used. See AUTOMATE report for references.
+% Resonances for planets after Mars (Jupiter, Saturn, ...) are arbitrary.
     
     if pl==1
         list_nm = [1 1; 6 5; 5 4; 4 3; 3 2];
@@ -9,6 +10,8 @@ function [list_nm] = find_resonance(pl)
         list_nm = [1 1; 2 1; 3 1; 2 3];
     elseif pl==4
         list_nm = [1 1; 2 1; 3 1];
+    else
+        list_nm = [1 1; 2 1; 3 1; 1 2; 1 3; 2 3; 3 2];
     end
 end
 
